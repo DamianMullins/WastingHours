4d21fc5b46f97316fa2c365b7d141dde685fb76f
Damian Mullins <damian@lowflyingowls.co.uk>
2013-10-28 11:10:02 +0000

    Added web ui test project using nunit & moq

---
+++ WastingHours.Tests/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using WastingHours.Controllers;
+
+namespace WastingHours.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index_HttpGet_ExpectedResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            ActionResult result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOf<ActionResult>(result);
+        }
+    }
+}
